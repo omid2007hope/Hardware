919c24fc5395d8b27b80e9ad47b312b0e7e8d920
omid2007hope <omidhope2007@gmail.com>
2026-08-09 06:17:41 +0200

    feat: implement architecture-specific spin locks and hardware register access with CPU capability detection

--- CPU/cpu.cpp
+++ CPU/cpu.cpp
@@ -5,6 +5,7 @@
 
 
 
+
 void spin_lock(std::atomic<bool>& flag) {
 	while (flag.exchange(true, std::memory_order_acquired)) {
 
@@ -16,4 +17,11 @@ void spin_lock(std::atomic<bool>& flag) {
 		std::this_thread::yield();
 #endif
 	};
+};
+
+void write_hardware_register(uintptr_t base_address, uint32_t value) {
+
+	auto* reg = reinterpret_cast<volatile uint32_t*>(base_address);
+
+	*reg = value;
 };
--- CPU/main.cpp
+++ CPU/main.cpp
@@ -10,7 +10,24 @@
 #include "cpu.hpp"
 
 
+#if defined(__x86_64__) || defined(_M_X64);
+#include <cpuid.h>
+bool has_avx2() {
+	unsigned int eax, ebx, ecx, edx;
+
+	if (__get_cpuid(1, &eax, &ebx, &ecx, &edx)) {
+		return (ecx & (1 << 20) != 0);
+	};
+
+	return false;
+};
+#endif
+
 int main() {
+#if defined(__x86_64__) || defined(_M_X64)
+	std::cout << "AVX Supported: " << (has_avx2() ? "Yes" : "No") << '\n';
+#endif
+
 	CPU_DATA_STD cpu;
 
 	unsigned int logicalCores_TOT = std::thread::hardware_concurrency();
