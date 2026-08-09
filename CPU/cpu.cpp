dc1819d6655078dc60651126d46291d4bd7bdb53
omid2007hope <omidhope2007@gmail.com>
2026-08-09 04:30:07 +0200

    feat: initialize hardware repository with CPU core monitoring utilities and VS Code configuration

---
+++ CPU/cpu.cpp
@@ -0,0 +1,19 @@
+#include "CPU.hpp"
+
+#include <atomic>
+#include <thread>
+
+
+
+void spin_lock(std::atomic<bool>& flag) {
+	while (flag.exchange(true, std::memory_order_acquired)) {
+
+#if defined(__x86_64__) || defined(_M_X64) __builtin_ia32_pause();
+
+#elif defined(__aarch64__) asm volatile("yield" :: : "memory");
+
+#else
+		std::this_thread::yield();
+#endif
+	};
+};
--- CPU/main.cpp
+++ CPU/main.cpp
@@ -7,14 +7,15 @@
 #include <iostream>
 #include <thread>
 
-#include "CPU.hpp"
+#include "cpu.hpp"
+
 
 int main() {
-  CPU_DATA_STD cpu;
+	CPU_DATA_STD cpu;
 
-  unsigned int logicalCores_TOT = std::thread::hardware_concurrency();
-  cpu.logicalCores_AVL = logicalCores_TOT;
+	unsigned int logicalCores_TOT = std::thread::hardware_concurrency();
+	cpu.logicalCores_AVL = logicalCores_TOT;
 
-  std::cout << "Available Logical CPU Cores: " << cpu.logicalCores_AVL;
-  return 0;
+	std::cout << "Available Logical CPU Cores: " << cpu.logicalCores_AVL;
+	return 0;
 };
